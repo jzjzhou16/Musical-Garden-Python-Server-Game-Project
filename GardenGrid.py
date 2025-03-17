42263814485d204a225d33c33e4fd9cd203d4e49
jzjzhou16 <jzjzhou16@gmail.com>
2025-03-17 13:00:17 -0400

    updated garden grid message

--- GardenGrid.py
+++ GardenGrid.py
@@ -13,8 +13,21 @@ class GardenGrid(MapObject):
         self.grid_rows = grid_rows
         self.grid_cols = grid_cols
         self.cell_size = cell_size
+
+        self.players_in_grid = set()
         # grid_state is a 2D list, each cell may only hold one plant
 
     def player_entered(self, player) -> list:
+        # if player is already in the grid, return no message
+        if player in self.players_in_grid:
+            return []
+        # else, add the player and send the welcome message
+        self.players_in_grid.add(player)
         return [DialogueMessage(self,player, "Welcome, You are in the Garden Grid.", "coin")]
+    
+    def player_exited(self, player) -> list:
+        # when a player leaves the grid, remove them from the method, so re-entry will trigger the message again
+        if player in self.players_in_grid:
+            self.players_in_grid.remove(player)
+        return []
     
