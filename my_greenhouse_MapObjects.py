5df191cf4660a859a853ab34f433db70dd18d6af
Finn Denton <132529100+finndenton@users.noreply.github.com>
2025-03-17 12:00:32 -0400

    Flyweight attributes updated with pngs

--- my_greenhouse_MapObjects.py
+++ my_greenhouse_MapObjects.py
@@ -1,14 +1,10 @@
 from typing import Dict
 
 class Plant:
-    def __init__(self, name: str, sound: int, image: str, size: tuple[int, int]):
+    def __init__(self, name: str, image: str, size: tuple[int, int]):
         self.name = name
-        self.sound = sound
         self.image = image  
         self.size = size
-
-    def play_sound(self):
-        return self.sound
 
 #Flyweight
 class PlantFactory:
@@ -17,13 +13,14 @@ class PlantFactory:
     def getPlant(name: str) -> Plant | None:
         if name not in PlantFactory._plants:
             plant_info = {
-                # fix these with actual data for each plant once found
-                "rose": Plant("rose", 1, "rose.png", (1, 1)),
-                "daisy": Plant("daisy", 2, "daisy.png", (1, 1)),
-                "lily": Plant("lily", 3, "lily.png", (1, 1)),
+                "Daisy": Plant("Daisy", "Daisy.png", (1, 1)),
+                "Lilac": Plant("Lilac", "Lilac.png", (1, 1)),
+                "Orchid": Plant("Orchid", "Orchid.png", (1, 1)),
+                "Rose": Plant("Rose", "Rose.png", (1, 1)),
+                "Sunflower": Plant("Sunflower", "Sunflower.png", (1, 1)),
+                "Tulip": Plant("Tulip", "Tulip.png", (1, 1)),
             }
             if name in plant_info:
                 PlantFactory._plants[name] = plant_info[name]
         return PlantFactory._plants.get(name)
                 
-
