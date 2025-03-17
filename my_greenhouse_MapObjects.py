d714c8f112872fd2c85dabd6619128ddcc54cd2d
Finn Denton <132529100+finndenton@users.noreply.github.com>
2025-03-17 13:22:19 -0400

    Update flyweight constructor for Plant

--- my_greenhouse_MapObjects.py
+++ my_greenhouse_MapObjects.py
@@ -1,5 +1,3 @@
-from .imports import *
-
 from typing import TYPE_CHECKING
 from typing import Dict
 
@@ -8,28 +6,34 @@ if TYPE_CHECKING:
     from maps.base import Map
     from tiles.base import MapObject
     from tiles.map_objects import *
+    from maps.base import Map
 
 
 
-class Plant():
-    def __init__(self, name: str, image: str, size: tuple[int, int]):
-        self.name = name
-        self.image = image  
-        self.size = size
+class Plant(MapObject):
+    def __init__(self, name: str, image: str) -> None:
+        super().__init__(f'plant/{image}', passable=True, z_index=1)
+        self.__name = name
+    
+    def _get_image_size(self) -> tuple[int, int]:
+        return (1,1)
+
+    def get_name(self) -> str:
+        return self.__name
 
 #Flyweight
 class PlantFactory:
     _plants: Dict[str, Plant] = {}
     @staticmethod
-    def getPlant(name: str) -> Plant | None:
+    def get_plant(name: str) -> Plant | None:
         if name not in PlantFactory._plants:
             plant_info = {
-                "Daisy": Plant("Daisy", "Daisy.png", (1, 1)),
-                "Lilac": Plant("Lilac", "Lilac.png", (1, 1)),
-                "Orchid": Plant("Orchid", "Orchid.png", (1, 1)),
-                "Rose": Plant("Rose", "Rose.png", (1, 1)),
-                "Sunflower": Plant("Sunflower", "Sunflower.png", (1, 1)),
-                "Tulip": Plant("Tulip", "Tulip.png", (1, 1)),
+                "Daisy": Plant("Daisy", "Daisy.png"),
+                "Lilac": Plant("Lilac", "Lilac.png"),
+                "Orchid": Plant("Orchid", "Orchid.png"),
+                "Rose": Plant("Rose", "Rose.png"),
+                "Sunflower": Plant("Sunflower", "Sunflower.png"),
+                "Tulip": Plant("Tulip", "Tulip.png"),
             }
             if name in plant_info:
                 PlantFactory._plants[name] = plant_info[name]
