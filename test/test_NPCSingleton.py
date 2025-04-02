2d79c0d5a05c623fd4e549a628396df1ac84f9b0
sydfrankk <yendysfrank@icloud.com>
2025-04-01 20:39:16 -0400

    Revert "Sydney changes"
    This reverts commit 7beee718cbf749addc80afaaf23b1da851e1dc7a.

--- test/test_NPCSingleton.py
+++ test/test_NPCSingleton.py
@@ -1,41 +1,41 @@
-# import pytest
+import pytest
 
-# from ..imports import *
-# from ..example_map import ExampleHouse
-# from ..GardenGrid import *
-# from ..NPCSingleton import *
-# from typing import TYPE_CHECKING 
+from imports import *
+from example_map import ExampleHouse
+from GardenGrid import *
+from NPCSingleton import *
+from typing import TYPE_CHECKING 
 
-# if TYPE_CHECKING:
-#     from coord import Coord
-#     from maps.base import Map
-#     from tiles.base import MapObject
-#     from tiles.map_objects import *
+if TYPE_CHECKING:
+    from coord import Coord
+    from maps.base import Map
+    from tiles.base import MapObject
+    from tiles.map_objects import *
 
-# @pytest.fixture
-# def garden_grid():
-#     """Create new Mock Garden Grid"""
-#     grid = GardenGrid("dirt3", Coord(2, 2), 4, 12)
-#     return grid
+@pytest.fixture
+def garden_grid():
+    """Create new Mock Garden Grid"""
+    grid = GardenGrid("dirt3", Coord(2, 2), 4, 12)
+    return grid
 
-# def test_singleton():
-#     """Test that NPC exhibits singleton behavior (only one instance should exist)"""
-#     npc1 = NPCSingleton(
-#         name="Professor",
-#         image="prof",
-#         encounter_text="Welcome!",
-#         grid = garden_grid.grid
-#     )
+def test_singleton():
+    """Test that NPC exhibits singleton behavior (only one instance should exist)"""
+    npc1 = NPCSingleton(
+        name="Professor",
+        image="prof",
+        encounter_text="Welcome!",
+        grid = garden_grid.grid
+    )
 
-#     npc2 = NPCSingleton(
-#         name="ShouldNotChange",
-#         image="wrong",
-#         encounter_text="This shouldn't appear",
-#         grid = garden_grid.grid
-#     )
+    npc2 = NPCSingleton(
+        name="ShouldNotChange",
+        image="wrong",
+        encounter_text="This shouldn't appear",
+        grid = garden_grid.grid
+    )
 
-#     assert npc1 is npc2  
-#     assert npc1.name == "Professor"   
-#     assert npc2.name == "Professor"   
-#     assert npc1.image == "prof"   
+    assert npc1 is npc2  
+    assert npc1.name == "Professor"   
+    assert npc2.name == "Professor"   
+    assert npc1.image == "prof"   
 
--- test/test_PlantFlyweight.py
+++ test/test_PlantFlyweight.py
@@ -1,38 +1,38 @@
-# from ..imports import *
-# import pytest
-# from ..Plant import Plant, PlantFactory
-# from typing import TYPE_CHECKING 
+from imports import *
+import pytest
+from ..Plant import Plant, PlantFactory
+from typing import TYPE_CHECKING 
 
-# if TYPE_CHECKING:
-#     from coord import Coord
-#     from maps.base import Map
-#     from tiles.base import MapObject
-#     from tiles.map_objects import *
+if TYPE_CHECKING:
+    from coord import Coord
+    from maps.base import Map
+    from tiles.base import MapObject
+    from tiles.map_objects import *
 
-# def test_flyweight_two_instances(self):
-#     """Test that two identical plant instances should be the same"""
-#     factory = PlantFactory()
-#     plant1 = factory.get_plant("Rose")
-#     plant2 = factory.get_plant("Rose")
-#     assert plant1 == plant2
+def test_flyweight_two_instances(self):
+    """Test that two identical plant instances should be the same"""
+    factory = PlantFactory()
+    plant1 = factory.get_plant("Rose")
+    plant2 = factory.get_plant("Rose")
+    assert plant1 == plant2
 
-# def test_invalid_plant(self):
-#     """Test that creating an invalid plant returns None"""
-#     factory = PlantFactory()
-#     plant = factory.get_plant("Invalid")
-#     assert plant is None
+def test_invalid_plant(self):
+    """Test that creating an invalid plant returns None"""
+    factory = PlantFactory()
+    plant = factory.get_plant("Invalid")
+    assert plant is None
 
-# def test_image_size(self):
-#     """Test that  the plant images are the correct size: 1x1"""
-#     factory = PlantFactory()
-#     plant = factory.get_plant("Rose")
-#     if plant is not None:
-#         assert plant._get_image_size() == (1,1)
+def test_image_size(self):
+    """Test that  the plant images are the correct size: 1x1"""
+    factory = PlantFactory()
+    plant = factory.get_plant("Rose")
+    if plant is not None:
+        assert plant._get_image_size() == (1,1)
 
-# def test_all_plant_types(self):
-#     """Test that all defined plant types can be created"""
-#     plant_names = ["Daisy", "Lilac", "Orchid", "Rose", "Sunflower", "Tulip"]
-#     for name in plant_names:
-#         plant = PlantFactory.get_plant(name)
-#         assert plant is not None
-#         assert plant.get_plant_name() == name
+def test_all_plant_types(self):
+    """Test that all defined plant types can be created"""
+    plant_names = ["Daisy", "Lilac", "Orchid", "Rose", "Sunflower", "Tulip"]
+    for name in plant_names:
+        plant = PlantFactory.get_plant(name)
+        assert plant is not None
+        assert plant.get_plant_name() == name
