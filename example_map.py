d78eae20dcd455fb39166d892b05fbbf1c16d481
sydfrankk <yendysfrank@icloud.com>
2025-04-02 21:09:36 -0400

    major image update
    background, path, etc.

--- example_map.py
+++ example_map.py
@@ -4,6 +4,7 @@ from .GridCellFactory import GridCellFactory
 from .NPCSingleton import NPCSingleton
 from typing import TYPE_CHECKING
 from .Plant import Plant, PlantFactory
+import random
 
 if TYPE_CHECKING:
     from coord import Coord
@@ -16,6 +17,7 @@ class ExampleHouse(Map):
     def __init__(self) -> None:
         #create garden grid 
         self.garden_grid = GardenGrid("dirt3", Coord(1,1), 4, 12)
+        
         # constructs NPC Singleton to have one instance of the grid exists at a time
         self.npc = NPCSingleton(
                 name="Professor",
@@ -23,17 +25,35 @@ class ExampleHouse(Map):
                 encounter_text="Welcome to the musical garden!",
                 grid=self.garden_grid
             )
+        
+        #background image options
+        self.background_options = [
+            'basicGrass',
+            'flowerGrass',   
+            'plantGrass'
+        ]
+
         super().__init__(
             name="Test House",
             description="Welcome to the Musical Garden",
             size=(15, 15),
             entry_point=Coord(14, 7),
-            background_tile_image='grassBackground',
+            background_tile_image = "basicGrass",
         )
-    
+
     def get_objects(self) -> list[tuple[MapObject, Coord]]:
         objects: list[tuple[MapObject, Coord]] = []
 
+        #randomize background tiles (flowers, plants, normal grass)
+        for y in range(15):   
+            for x in range(15):   
+                if (y, x) in [(14, 7)]:
+                    continue
+                    
+                bg_image = random.choice(self.background_options)
+                bg_tile = Background(bg_image)
+                objects.append((bg_tile, Coord(y, x)))
+        
         # add a door
         door = Door('int_entrance', linked_room="Trottier Town")
         objects.append((door, Coord(14, 7)))
@@ -84,6 +104,32 @@ class ExampleHouse(Map):
                 cell = tilemap[i][j]
                 cell_coord = Coord(grid_origin.y + i, grid_origin.x + j)  
                 objects.append((cell, cell_coord))
+
+        #add paths:
+        path_types = {
+            'main': 'path',
+            'left side': "left", 
+            'right side': "right", 
+            'top left corner': "topLeft", 
+            'top right corner': "topRight", 
+            'bottom right corner': "bottomRight", 
+            'bottom left corner': "bottomLeft", 
+            'bottom': "bottom",
+            'top': "top",
+            'stones': "stoneGrass"
+        }
+
+        path_coords = []
+
+        for y in range(4, 14):
+            objects.append((Background(path_types['left side']), Coord(y, 6)))
+            objects.append((Background(path_types['main']), Coord(y, 7)))
+            objects.append((Background(path_types['main']), Coord(y, 8)))
+            objects.append((Background(path_types['right side']), Coord(y, 9)))
+
+        for x in range(6, 10):
+            objects.append((Background(path_types['top']), Coord(4, x)))   
+            objects.append((Background(path_types['bottom']), Coord(13, x)))  
 
         return objects
 
