ab241c9da06ab7c6945c242b58d7ce6afcdd5d7d
Finn Denton <132529100+finndenton@users.noreply.github.com>
2025-04-05 15:59:54 -0400

    Pressure Plate fixed to extend PressurePlate

--- PressurePlate.py
+++ PressurePlate.py
@@ -5,38 +5,38 @@ from .GridManager import GridManager
 if TYPE_CHECKING:
     from message import Message, SoundMessage
     from GridManager import GridManager
-    from tiles.map_objects import UtilityObject
+    from tiles.map_objects import PressurePlate
 
-class ColumnPressurePlate(UtilityObject):
+class ColumnPressurePlate(PressurePlate):
     def __init__(self, column_index: int):
-        super().__init__(f"pressure_plate", passable=True)
+        super().__init__(image_name='pressure_plate', stepping_text='')
         self.__active = False
         self.column_index = column_index
 
-    def update(self) -> List[Message]:
-        return []
-
-    def player_entered(self, player) -> List[SoundMessage]:
+    def player_entered(self, player) -> List['SoundMessage']:
+        messages = []
+        
         if not self.__active:
             self.__active = True
             manager = GridManager.get_instance()
             
             if not manager:
-                return []
+                return messages
             
             for row in range(4):
                 plant_name = manager.notes_grid[row][self.column_index]
                 if plant_name and plant_name.lower() in GridManager.PLANT_NOTES:
                     note = GridManager.PLANT_NOTES[plant_name.lower()]
                     octave = GridManager.ROW_OCTAVES[row]
-                    return [SoundMessage(
+                    messages.append(SoundMessage(
                         player,
                         f"{note}{octave}.mp3",
                         volume=0.7,
                         repeat=False
-                    )]
-        return []
+                    ))
+        
+        return messages
 
-    def player_exited(self, player) -> List[Message]:
+    def player_exited(self, player) -> List['Message']:
         self.__active = False
         return []
