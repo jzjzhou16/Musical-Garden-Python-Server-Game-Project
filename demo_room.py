dab809937a73cb40873afe9dd7da68d872ad8cf7
sydfrankk <yendysfrank@icloud.com>
2025-04-05 19:01:14 -0400

    demo_room push

--- demo_room.py
+++ demo_room.py
@@ -3,7 +3,7 @@ from .GardenGrid import *
 from typing import TYPE_CHECKING
 from .Plant import PlantFactory 
 from .BackgroundType import BackgroundFactory
-
+from .PressurePlate import ColumnPressurePlate
 
 if TYPE_CHECKING:
     from coord import Coord
@@ -15,15 +15,15 @@ class DemoRoom(Map):
     def __init__(self) -> None:
         #create garden grids
         self.demo_grids = [
-            GardenGrid("dirt3", Coord(1, 1), 4, 12),
-            GardenGrid("dirt3", Coord(6, 1), 4, 12),
-            GardenGrid("dirt3", Coord(11, 1), 4, 12)
+            GardenGrid("dirt3", Coord(2, 1), 4, 12),
+            GardenGrid("dirt3", Coord(8, 1), 4, 12),
+            GardenGrid("dirt3", Coord(14, 1), 4, 12)
         ]
 
         super().__init__(
             name="Demo House",
-            size=(18, 16),
-            entry_point=Coord(16, 1),
+            size=(21, 16),
+            entry_point=Coord(19, 1),
             description="Welcome to the Musical Garden Demo Room",
             background_tile_image = "basicGrass", 
         )
@@ -60,31 +60,30 @@ class DemoRoom(Map):
         }, 
         # Demo 2 layout - twinkle twinkle little star
         { 
-            Coord(13, 1): "Rose",    # A 
-            Coord(13, 3): "Orchid",  # G
-            Coord(13, 4): "Orchid",  # G 
-
-            Coord(13, 6): "Daisy",   # C
-            Coord(13, 7): "Daisy",   # C
-            Coord(13, 8): "Orchid",  # G
-            Coord(13, 9): "Orchid",  # G
-            Coord(13, 10): "Rose",   # A
-            Coord(13, 11): "Rose",   # A
-            Coord(13, 12): "Orchid", # G
+            # Coord(10, 1): "Rose",    # A 
+            # Coord(10, 3): "Orchid",  # G
+            # Coord(10, 4): "Orchid",  # G 
+            Coord(10, 1): "Daisy",   # C
+            Coord(10, 2): "Daisy",   # C
+            Coord(10, 3): "Orchid",  # G
+            Coord(10, 4): "Orchid",  # G
+            Coord(10, 5): "Rose",   # A
+            Coord(10, 6): "Rose",   # A
+            Coord(10, 7): "Orchid", # G
         }, 
         # Demo 3 layout - jingle bells
         { 
-            Coord(8, 1): "Iris",     # E
-            Coord(8, 2): "Iris",     # E
-            Coord(8, 3): "Iris",     # E
-            Coord(8, 4): "Iris",     # E
-            Coord(8, 6): "Iris",     # E
-            Coord(8, 7): "Iris",     # E
-            Coord(8, 8): "Iris",     # E 
-            Coord(8, 9): "Orchid",   # G
-            Coord(8, 10): "Daisy",     # C 
-            Coord(8, 11): "Sunflower", # D
-            Coord(8, 12): "Iris",    # E
+            Coord(15, 1): "Iris",     # E
+            Coord(15, 2): "Iris",     # E
+            Coord(15, 3): "Iris",     # E
+            Coord(15, 4): "Iris",     # E
+            Coord(15, 6): "Iris",     # E
+            Coord(15, 7): "Iris",     # E
+            Coord(15, 8): "Iris",     # E 
+            Coord(15, 9): "Orchid",   # G
+            Coord(15, 10): "Daisy",     # C 
+            Coord(15, 11): "Sunflower", # D
+            Coord(15, 12): "Iris",    # E
         }
         ]
 
@@ -95,10 +94,10 @@ class DemoRoom(Map):
                     objects.append((plant, coord))
 
         #create background
-        for y in range(18):
+        for y in range(20):
             for x in range(16):
-                if (y, x) == (14, 7):  
-                    continue
+                # if (y, x) == (14, 7):  
+                #     continue
             
                 #set randomized background w/ flyweight factory
                 background_tile = BackgroundFactory.get_background()
@@ -106,21 +105,32 @@ class DemoRoom(Map):
 
         # add a exit door back to room
         door = Door('houseDoor', linked_room="Example House", is_main_entrance=True)
-        objects.append((door, Coord(16, 1)))
+        objects.append((door, Coord(19, 1)))
     
         #demo 1
         demo1Sign = Sign('sign', "Demo Track: Happy Birthday")
         demo1PlayButton = ExtDecor('playButton')
+        objects.append((demo1Sign, Coord(5,14)))
 
-        objects.append((demo1Sign, Coord(3,14)))
+        for col in range(12):
+            plate = ColumnPressurePlate(col)
+            objects.append((plate, Coord(x=col + 1, y=1)))
 
         #demo 2
         demo2Sign = Sign('sign', "Demo Track: Twinkle Twinkle Little Star")
-        objects.append((demo2Sign, Coord(8,14)))
+        objects.append((demo2Sign, Coord(10,14)))
+
+        for col in range(12):
+            plate = ColumnPressurePlate(col)
+            objects.append((plate, Coord(x=col + 1, y=7)))
 
         #demo 3
         demo3Sign = Sign('sign', "Demo Track: jingle bells")
-        objects.append((demo3Sign, Coord(13,14)))
+        objects.append((demo3Sign, Coord(15,14)))
+
+        for col in range(12):
+            plate = ColumnPressurePlate(col)
+            objects.append((plate, Coord(x=col + 1, y=13)))
 
         return objects
      
