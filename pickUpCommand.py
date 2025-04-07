00cd177c2d31ddfa8e98d53d2619843411061cba
Finn Denton <132529100+finndenton@users.noreply.github.com>
2025-04-07 15:21:00 -0400

    removed lower()

--- pickUpCommand.py
+++ pickUpCommand.py
@@ -19,7 +19,7 @@ class pickUpPlantCommand(ChatCommand):
         messages = []
         messages.append(DialogueMessage(self, player, f"You picked up {plant_name}!", ""))
         from .GridManager import GridManager
-        note = GridManager.PLANT_NOTES[plant_name.lower()]
+        note = GridManager.PLANT_NOTES[plant_name]
         # Default preview note is A2, B2... etc
         messages.append(SoundMessage(player, f"{note}_2.mp3", volume = 1.0, repeat = False))
 
